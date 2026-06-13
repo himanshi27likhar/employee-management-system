a10ba3b67ddd9d10899ab4e9b3e93df6b8c5f1ae
himanshi27likhar <himanshi.likhar.8@gmail.com>
2026-06-14 00:55:20 +0530

    fix case sensitivity issues

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import EditEmployee from "./pages/EditEmployee";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
-import Signup from "./pages/signin";
+import Signup from "./pages/Signin";
 import Dashboard from "./pages/Dashboard";
 import CreateEmployee from "./pages/CreateEmployee";
 import EmployeeList from "./pages/EmployeeList";
-import Dashboard from "./pages/Dashboard";
+import EditEmployee from "./pages/EditEmployee";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
         {/* Auth pages */}
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
@@ -21,7 +21,7 @@ function App() {
         {/* Employee features */}
         <Route path="/create-employee" element={<CreateEmployee />} />
         <Route path="/employees" element={<EmployeeList />} />
-
+        <Route path="/edit-employee/:id" element={<EditEmployee />} />
       </Routes>
     </BrowserRouter>
   );
